656453c39c73410f07672472805452f3babcc042
DareIsrael <dareisrael4@gmail.com>
2024-03-12 00:48:39 -0700

    Third commit

--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,15 @@ const router = express.Router()
 
 
 // update user
-router.put("/:id", verifyUser, updateUser);
+router.put("/:id",  updateUser);
 
 // delete user
-router.delete("/:id", verifyUser, deleteUser);
+router.delete("/:id",  deleteUser);
 
 // get single user
-router.get("/:id", verifyUser, getSingleUser);
+router.get("/:id",  getSingleUser);
 
 // get all users
-router.get("/", verifyAdmin, getAllUser);
+router.get("/",  getAllUser);
 
 export default router
